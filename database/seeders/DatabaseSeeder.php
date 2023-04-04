63ae9748634237027d047d6691a10a0729e438fc
Andhika Raharja <andhikarmukti@gmail.com>
2023-04-05 01:48:19 +0700

    finished with seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,5 +26,17 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(RoleSeeder::class);
+        $this->call(PermissionSeeder::class);
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('asdasdasd'),
+            'is_approved' => 1,
+        ]);
+        $user->assignRole('admin');
+
+        $permission = Permission::where('name', 'access_api')->first();
+        $user->givePermissionTo($permission);
     }
 }
